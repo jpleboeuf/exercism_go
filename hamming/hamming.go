3c1854f0950fbf86272390ff7fc22a6e3261825b
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-12-16 21:17:53 +0100

    Solve exercise "Hamming" (cont.)
    Extracted common idioms to their own functions.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -9,25 +9,48 @@ import (
 		"errors"
 	)
 
+func zip(s1, s2 string) ([][2]string, error) {
+
+        if len(s1) != len(s2) {
+                return nil, errors.New("strings not of equal length")
+        }
+
+        zipS := make([][2]string, len(s1))
+        for i := range zipS {
+                zipS[i] = [2]string{string(s1[i]), string(s2[i])}
+        }
+
+        return zipS, nil
+}
+
+func countIf(bools []bool, countCond func(bool) bool) int {
+        c := 0
+        for _, v := range bools {
+                if countCond(v) {
+                        c++
+                }
+        }
+        return c
+}
+
+func nFalse(bools []bool) int {
+        return countIf(bools, func(b bool) bool { return b == false })
+}
+
 // Distance returns,
 //  given 2 strings representing 2 strands of DNA,
 //  the Hamming Distance between them.
 func Distance(a, b string) (int, error) {
 
-	if len(a) != len(b) {
-		return -1, errors.New("sequences not of equal length")
-	}
+	zipAB, err := zip(a, b)
+        if err != nil {
+               return -1, errors.New("sequences not of equal length")
+        }
 
-	zipAB := make([][2]string, len(a))
 	zIdent := make([]bool, len(zipAB))
-	d := 0
 	for i := range a {
-		zipAB[i] = [2]string{string(a[i]), string(b[i])}
 		zIdent[i] = (zipAB[i][0] == zipAB[i][1])
-		if zIdent[i] == false {
-			d++
-		}
 	}
 
-	return d, nil;
+	return nFalse(zIdent), nil;
 }
